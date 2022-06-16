1257a7d103b0f39400c82f76be634dabffa912c4
Adam Alrasi <30912563+adamalrasi@users.noreply.github.com>
2022-06-16 22:41:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ numberInput.addEventListener('input', getFactFetch);
 //     let number = numberInput.value;
 
 //     let xhr = new XMLHttpRequest();
-//     xhr.open('GET', 'http://numbersapi.com/' + number);
+//     xhr.open('GET', 'https://numbersapi.com/' + number);
 
 //     xhr.onload = function () {
 //         if (this.status === 200 && number != '') {
